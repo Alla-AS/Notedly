b58e4f3bc0340560830dd534eb03cbc61dc295ca
Alla-AS <705saa@mail.ru>
2023-09-20 18:25:40 +0300

    add data and authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
-
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // Импортируем локальные модули
@@ -8,6 +8,7 @@ const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+
 
 // Запускаем сервер на порте, указанном в файле .env, или на порте 4000
 const port = process.env.PORT || 4000;
@@ -19,13 +20,32 @@ const app = express();
 db.connect(DB_HOST);
 
 
+  // Получаем информацию пользователя из JWT
+  const getUser = token => {
+    if (token) { 
+      try {
+// Возвращаем информацию пользователя из токена 
+        return jwt.verify(token, process.env.JWT_SECRET);
+      } catch (err) {
+// Если с токеном возникла проблема, выбрасываем ошибку 
+       new Error('Session invalid');
+      }
+    }
+  };
+
 // Настройка Apollo Server
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => {
-      // Добавление моделей БД в context 
-      return { models };
+    context: ({ req }) => {
+        // Получаем токен пользователя из заголовков
+        const token = req.headers.authorization;
+        // Пытаемся извлечь пользователя с помощью токена
+        const user = getUser(token);
+        // Пока что будем выводить информацию о пользователе в консоль:
+        console.log(user);
+        // Добавляем модели БД и пользователя в контекст 
+        return { models, user };
     }
 });
 
--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,8 +1,65 @@
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken'); 
+const {
+  AuthenticationError, 
+  ForbiddenError
+} = require('apollo-server-express'); 
+require('dotenv').config();
+const gravatar = require('../util/gravatar');
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
       content: args.content,
       author: 'Adam Scott'
     });
+  },
+
+  deleteNote: async (parent, {id}, {models}) => {
+    try {
+      await models.Note.findOneAndRemove({ _id: id});
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+  
+  updateNote: async (parent, {content, id}, {models}) => {
+    return await models.Note.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          content
+        }
+      },
+      {
+        new: true
+      }
+    );
+  },
+
+  signUp: async(parent, {username, email, password}, {models}) => {
+    email = email.trim().toLowerCase();
+
+    // Хешируем пароль с помощью доп пакета bcrypt
+    const hashed = await bcrypt.hash(password, 10);
+    // Создаем url gravatar-изображения
+    const avatar = gravatar(email); 
+    try {
+      const user = await models.User.create({ 
+        username,
+        email, 
+        avatar,
+        password: hashed
+      });
+  // Создаем и возвращаем json web token
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) { 
+      console.log(err);
+  // Если при регистрации возникла проблема, выбрасываем ошибку 
+      throw new Error('Error creating account');
+    }
   }
 };
